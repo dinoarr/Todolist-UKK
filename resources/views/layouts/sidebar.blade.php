c1e2ba8cd6dc7a226bb5812fa69a4c79be0ebd4c
dinoarr <dinoananda.restyanto@gmail.com>
2025-03-13 22:07:33 +0700

    feat: add new name for aplication and add route

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -4,18 +4,18 @@
     <div class="sidebar-custom shadow">
         <div class="sidebar-header">
             <div class="logo">
-                <span class="menu-text">TODOLIST</span>
+                <span class="menu-text">Taskly</span>
             </div>
         </div>
-        <a href="" class="menu-item active">
+        <a href="{{ route('dashboard') }}" class="menu-item {{ request()->routeIs('dashboard') ? 'active' : '' }}">
             <i class="fas fa-house-chimney"></i>
             <span class="menu-text">Dashboard</span>
         </a>
-        <a href="" class="menu-item ">
+        <a href="{{ route('tasks.index') }}" class="menu-item {{ request()->routeIs('tasks.*') ? 'active' : '' }}">
             <i class="fas fa-clipboard-list"></i>
             <span class="menu-text">Tasks</span>
         </a>
-        <a href="" class="menu-item ">
+        <a href="{{ route('pages.completed') }}" class="menu-item {{ request()->routeIs('pages.*') ? 'active' : '' }}">
             <i class="fas fa-clipboard-check"></i>
             <span class="menu-text">Completed Tasks</span>
         </a>
