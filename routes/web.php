3fba39ac4380d257775eef64d75fb0f3436783b8
dinoarr <dinoananda.restyanto@gmail.com>
2025-03-13 22:09:21 +0700

    feat(route): add new route for pages and tasks

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('pages.dashboard');
-});
+Route::get('/', [PagesController::class, 'dashboard'])->name('dashboard');
+Route::get('completed', [PagesController::class, 'completed'])->name('pages.completed');
+
+Route::get('tasks', [TaskController::class, 'index'])->name('tasks.index');
+Route::get('tasks/create', [TaskController::class, 'create'])->name('tasks.create');
+Route::get('tasks/edit', [TaskController::class, 'edit'])->name('tasks.edit');
